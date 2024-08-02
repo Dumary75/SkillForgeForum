2db326dcc3542a502ae2542f9d69b931e17028b5
Dumary75 <113781200+Dumary75@users.noreply.github.com>
2024-08-02 18:17:15 +0200

    Almost completed the index page

--- JS_Files/index.js
+++ JS_Files/index.js
@@ -4,9 +4,22 @@
 
 let header_default = document.querySelector('header');
 let header_button = document.querySelector('header div');
-let main_blocks = document.querySelectorAll('main div');
+let main_blocks = document.querySelectorAll('Main a div');
+let header_blocks = document.querySelectorAll('.header_blocks_default');
+let header_headline = document.querySelector('.header_headline');
+let footer = document.querySelector('footer');
 
 header_button.addEventListener('click', () => {
        header_default.classList.toggle('pressed');
-       main_blocks.classList.toggle('pressed_div');
-});
+       header_headline.classList.toggle('header_headline_pressed');
+       header_blocks.forEach(header_block => {
+              header_block.classList.toggle('pressed_header_blocks');
+       });
+       main_blocks.forEach(main_block => {
+              main_block.classList.toggle('pressed_div');
+       });
+       footer.classList.toggle('footer_pressed');
+   });
+
+
+   
