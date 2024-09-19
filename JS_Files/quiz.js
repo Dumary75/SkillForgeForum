39ca6cc346dbb46a7e3ecb78ed87d8b1d10e05aa
Dumary75 <113781200+Dumary75@users.noreply.github.com>
2024-09-19 12:17:39 +0200

    Start working on quiz_mobil page

--- JS_Files/quiz.js
+++ JS_Files/quiz.js
@@ -13,10 +13,10 @@
        };
  });
 
-// Header Click Event //
+// --- Header Click Event --- //
 
 let header_default = document.querySelector('header');
-let main_blocks = document.querySelectorAll('.main_blocks');
+let main_container = document.querySelector('.main_container');
 let header_blocks = document.querySelectorAll('.header_blocks_default');
 let header_headline = document.querySelector('.header_headline');
 let footer = document.querySelector('footer');
@@ -29,12 +29,10 @@ header_headline.addEventListener('click', () => {
               header_block.classList.toggle('pressed_header_blocks');
        });
 
-       main_blocks.forEach(main_block => {
-              main_block.classList.toggle('pressed_blurry_effect');
-       });
-       
+       main_container.classList.toggle('pressed_blurry_effect');
        footer.classList.toggle('pressed_blurry_effect');
    });
 
 
-   
+
+//-----------------------------------------------------------------------//
