f996fd3c84ed25a8a6a7b35fede38307723b2e26
Dumary75 <113781200+Dumary75@users.noreply.github.com>
2024-10-08 11:27:04 +0200

    Added functions for the login_state_button

--- JS_Files/index.js
+++ JS_Files/index.js
@@ -1,15 +1,5 @@
 "use strict"
 
- // > Account_current_state Logic //  
- let account_state = document.querySelector('.login_state');
- window.addEventListener('load', () => {
-       let acc_token = JSON.parse(localStorage.getItem("loginToken"))
-       if(acc_token){
-        account_state.classList.add('login_state_loggedIn');
-         account_state.innerText = 'eingeloggt';    
-       };
- });
-
 // Header Click Event //
 
 let header_default = document.querySelector('header');
@@ -32,6 +22,60 @@ header_headline.addEventListener('click', () => {
        
        // footer.classList.toggle('pressed_blurry_effect');
    });
+
+// > Account_current_state Logic //  
+let account_state = document.querySelector('.login_state');
+let account_state_clicked = document.querySelector('.login_state_clicked');
+let ausloggen = document.querySelector('.ausloggen');
+let ausloggen_abrechen = document.querySelector('.abrechen');
+let acc_token = JSON.parse(localStorage.getItem("loginToken"))
+
+window.addEventListener('load', () => {
+       if(acc_token){
+              account_state.classList.add('login_state_loggedIn');
+              account_state.innerText = 'eingeloggt';
+       } else {
+              ausloggen.innerText = 'einloggen';
+              ausloggen.style.backgroundColor = '#0cb11a';
+       }
+});
+ 
+
+account_state.addEventListener('click', () => {
+              account_state_clicked.classList.toggle('login_state_clicked_active');
+              header_headline.classList.toggle('pressed_blurry_effect');
+              header_blocks.forEach(header_block => {
+              header_block.classList.toggle('pressed_blurry_effect'); });                                       
+       });
+
+ausloggen.addEventListener('click', () => {
+       let acc_token = JSON.parse(localStorage.getItem("loginToken"))
+       if(acc_token){
+              account_state.classList.remove('login_state_loggedIn');
+              account_state.innerText = 'ausgeloggt'; 
+              ausloggen.innerText = 'einloggen';
+              ausloggen.style.backgroundColor = '#0cb11a';
+              localStorage.removeItem('loginToken');
+              alert('Erfolgreich ausgeloggt!');
+              account_state_clicked.classList.remove('login_state_clicked_active');
+              header_headline.classList.remove('pressed_blurry_effect');
+              header_blocks.forEach(header_block => {
+              header_block.classList.remove('pressed_blurry_effect'); });                              
+       } else {
+              window.location.href = 'account.html';
+       };
+   });       
+
+ausloggen_abrechen.addEventListener('click', () => {
+              account_state_clicked.classList.remove('login_state_clicked_active');
+              header_headline.classList.remove('pressed_blurry_effect');
+              header_blocks.forEach(header_block => {
+              header_block.classList.remove('pressed_blurry_effect');
+                                                 });
+       });        
+   
+
+    
 
 
    
--- JS_Files/quiz.js
+++ JS_Files/quiz.js
@@ -1,22 +1,12 @@
 "use strict"
 
- // > Account_current_state Logic //  
- let account_state = document.querySelector('.login_state');
- window.addEventListener('load', () => {
-       let acc_token = JSON.parse(localStorage.getItem("loginToken"))
-       if(acc_token){
-        account_state.classList.add('login_state_loggedIn');
-         account_state.innerText = 'eingeloggt';    
-       };
- });
-
 // --- Header Click Event --- //
 
 let header_default = document.querySelector('header');
 let main_container = document.querySelector('.main_container');
 let header_blocks = document.querySelectorAll('.header_blocks_default');
 let header_headline = document.querySelector('.header_headline');
-// let footer = document.querySelector('footer');
+let footer = document.querySelector('footer');
 
 header_headline.addEventListener('click', () => {
        header_default.classList.toggle('pressed');
@@ -27,10 +17,60 @@ header_headline.addEventListener('click', () => {
        });
 
        main_container.classList.toggle('pressed_blurry_effect');
-    //    footer.classList.toggle('pressed_blurry_effect');
+       // footer.classList.toggle('pressed_blurry_effect');
    });
 
+// > Account_current_state Logic < //  
 
+let account_state = document.querySelector('.login_state');
+let account_state_clicked = document.querySelector('.login_state_clicked');
+let ausloggen = document.querySelector('.ausloggen');
+let ausloggen_abrechen = document.querySelector('.abrechen');
+let acc_token = JSON.parse(localStorage.getItem("loginToken"))
+
+window.addEventListener('load', () => {
+       if(acc_token){
+              account_state.classList.add('login_state_loggedIn');
+              account_state.innerText = 'eingeloggt';
+       } else {
+              ausloggen.innerText = 'einloggen';
+              ausloggen.style.backgroundColor = '#0cb11a';
+       }
+});
+ 
+
+account_state.addEventListener('click', () => {
+              account_state_clicked.classList.toggle('login_state_clicked_active');
+              header_headline.classList.toggle('pressed_blurry_effect');
+              header_blocks.forEach(header_block => {
+              header_block.classList.toggle('pressed_blurry_effect'); });                                       
+       });
+
+ausloggen.addEventListener('click', () => {
+       let acc_token = JSON.parse(localStorage.getItem("loginToken"))
+       if(acc_token){
+              account_state.classList.remove('login_state_loggedIn');
+              account_state.innerText = 'ausgeloggt'; 
+              ausloggen.innerText = 'einloggen';
+              ausloggen.style.backgroundColor = '#0cb11a';
+              localStorage.removeItem('loginToken');
+              alert('Erfolgreich ausgeloggt!');
+              account_state_clicked.classList.remove('login_state_clicked_active');
+              header_headline.classList.remove('pressed_blurry_effect');
+              header_blocks.forEach(header_block => {
+              header_block.classList.remove('pressed_blurry_effect'); });                              
+       } else {
+              window.location.href = 'account.html';
+       };
+   });       
+
+ausloggen_abrechen.addEventListener('click', () => {
+              account_state_clicked.classList.remove('login_state_clicked_active');
+              header_headline.classList.remove('pressed_blurry_effect');
+              header_blocks.forEach(header_block => {
+              header_block.classList.remove('pressed_blurry_effect');
+                                                 });
+       });        
 
 //-----------------------MAIN_CONTENT-------------------------------------------//
 
