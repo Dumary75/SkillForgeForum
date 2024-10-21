31ee6fe079b27293e4605b6f136b9f1e0b63ea4c
Dumary75 <113781200+Dumary75@users.noreply.github.com>
2024-10-21 14:55:22 +0200

    Worked on the acc_state_logic

--- JS_Files/weather.js
+++ JS_Files/weather.js
@@ -4,35 +4,13 @@
 let acc_token = JSON.parse(localStorage.getItem("loginToken"))
 
 // --- Header Click Event --- //
-
 let header_default = document.querySelector('header');
 let main_container = document.querySelector('.main_container');
-let main_container_game = document.querySelector('.main_container_game');
 let header_blocks = document.querySelectorAll('.header_blocks_default');
 let header_headline = document.querySelector('.header_headline');
 let footer = document.querySelector('footer');
 
-if(acc_token){
-    header_headline.addEventListener('click', () => {
-        header_default.classList.toggle('pressed');
-        header_headline.classList.toggle('header_headline_pressed');
- 
-        header_blocks.forEach(header_block => {
-               header_block.classList.toggle('pressed_header_blocks');
-        });
- 
-        if(main_container_game){
-            main_container_game.classList.toggle('pressed_blurry_effect');
-        } else if(main_container){
-            main_container.classList.toggle('pressed_blurry_effect');
-        };
-
-        footer.classList.toggle('pressed_blurry_effect');
-    });
-}
-
 // > Account_current_state Logic < //  
-
 let account_state = document.querySelector('.login_state');
 let account_state_clicked = document.querySelector('.login_state_clicked');
 let ausloggen = document.querySelector('.ausloggen');
@@ -42,67 +20,70 @@ let logged_site_blocker = document.querySelector('.login_checker');
 let logged_site_blocker_boxes = document.querySelectorAll('.login_checker_boxes');
 
 window.addEventListener('load', () => {
-       if(acc_token){
-              account_state.classList.add('login_state_loggedIn');
-              account_state.innerText = 'eingeloggt';
-       } else {
-        logged_site_blocker.classList.add('login_checker_active');
-        header_headline.classList.add('pressed_blurry_effect');
-        main_container.classList.add('pressed_blurry_effect');
-        logged_site_blocker_boxes.forEach(box => {
-            box.addEventListener('click', () => {
-                window.location.href = 'account.html';
+    if(acc_token){
+           account_state.classList.add('login_state_loggedIn');
+           account_state.innerText = 'eingeloggt';
+
+           header_headline.addEventListener('click', () => {
+           header_default.classList.toggle('pressed');
+           header_headline.classList.toggle('header_headline_pressed');
+     
+           header_blocks.forEach(header_block => {
+                   header_block.classList.toggle('pressed_header_blocks');
             });
+     
+            main_blurry_effect();
         });
-       };
+
+    } else {
+     logged_site_blocker.classList.add('login_checker_active');
+     logged_site_blocker_boxes.forEach(box => {
+         box.addEventListener('click', () => {
+             window.location.href = 'account.html';
+         });
+     });
+     header_blurr_effect();
+     main_blurry_effect();
+    };
 });
- 
+
+// Blurry_Effects
+function main_blurry_effect(){
+        main_container.classList.toggle('pressed_blurry_effect');
+        footer.classList.toggle('pressed_blurry_effect');  
+};
+
+function header_blurr_effect(){
+    account_state_clicked.classList.toggle('login_state_clicked_active');
+    header_blocks.forEach(header_block => {
+    header_block.classList.toggle('pressed_blurry_effect'); });
+    if(window.innerWidth <= 991){
+        header_headline.classList.toggle('pressed_blurry_effect');
+    };
+};
 
 account_state.addEventListener('click', () => {
-              account_state_clicked.classList.toggle('login_state_clicked_active');
-              header_headline.classList.toggle('pressed_blurry_effect');
-              header_blocks.forEach(header_block => {
-              header_block.classList.toggle('pressed_blurry_effect'); });     
-              if(main_container_game){
-                main_container_game.classList.toggle('pressed_blurry_effect');
-            } else if(main_container){
-                main_container.classList.toggle('pressed_blurry_effect');
+            if(window.innerWidth > 991){
+                header_blurr_effect();
+                main_blurry_effect();
+            } else {
+                header_blurr_effect();
             };
-            footer.classList.toggle('pressed_blurry_effect');
-                                    
-       });
+     }); 
 
 ausloggen.addEventListener('click', () => {
-       let acc_token = JSON.parse(localStorage.getItem("loginToken"))
-       if(acc_token){
-              account_state.classList.remove('login_state_loggedIn');
-              account_state.innerText = 'ausgeloggt'; 
-              ausloggen.innerText = 'einloggen';
-              ausloggen.style.backgroundColor = '#0cb11a';
               localStorage.removeItem('loginToken');
               alert('Erfolgreich ausgeloggt!');
-              account_state_clicked.classList.remove('login_state_clicked_active');
-              header_headline.classList.remove('pressed_blurry_effect');
-              header_blocks.forEach(header_block => {
-              header_block.classList.remove('pressed_blurry_effect'); });                              
-       } else {
-              window.location.href = 'account.html';
-       };
+              window.location.reload()
    });       
 
 ausloggen_abrechen.addEventListener('click', () => {
-              account_state_clicked.classList.remove('login_state_clicked_active');
-              header_headline.classList.remove('pressed_blurry_effect');
-              header_blocks.forEach(header_block => {
-              header_block.classList.remove('pressed_blurry_effect');
-                                                 });
-                                                 if(main_container_game){
-                                                    main_container_game.classList.toggle('pressed_blurry_effect');
-                                                } else if(main_container){
-                                                    main_container.classList.toggle('pressed_blurry_effect');
-                                                };
-              footer.classList.toggle('pressed_blurry_effect');
-                                        
+    if(window.innerWidth > 991){
+        header_blurr_effect();
+        main_blurry_effect();
+        } else {
+            header_blurr_effect();
+    };                             
        });    
 
 //-----------------------MAIN_CONTENT-------------------------------------------//
